754c0a7e70887c5164c814f011e144b76d90df42
Ayush Shah <shahayush934@gmail.com>
2023-12-29 22:39:53 +0530

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,20 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by ayush on 29/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
